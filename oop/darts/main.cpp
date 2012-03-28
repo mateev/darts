80e94d83d4e64d351ed6e55f6588dad2f843e92b
Ivan Mateev <ivan.p.mateev@gmail.com>
2012-03-28 08:46:34 +0100

    OOP: Game Object Continued

--- oop/darts/main.cpp
+++ oop/darts/main.cpp
@@ -17,13 +17,25 @@ void printPracticeStatistics(long gamesCount)
 {
 	Game practiceSession = Game();
 
+	int* records = new int[Game::MAX_GAMES_COUNT+1];
+
+	for(int i = 0;i<=Game::MAX_GAMES_COUNT;i++)
+		records[i] = 0;
+
+	for(int i = 0;i<gamesCount;i++)
+		records[practiceSession.Practice()]++;
+
+	for(int i = 0;i<=Game::MAX_GAMES_COUNT;i++)
+		if(records[i]!=0)
+			cout << i << ' ' << records[i] << endl;
+	/*
 	for(int i =0;i<gamesCount;i++)
 	{
 		if(practiceSession.Practice()<=6)
 			cout << "J" << i;
 		cout << i;
 	}
-
+	*/
 	cin.get();
 
 }
